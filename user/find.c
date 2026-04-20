9b36f6b33a7054c7b7ea01f84d2b18e2244be3ae
Louis Deng <louisaeilotd@gmail.com>
2026-04-20 22:01:08 +0800

    Update find.c with -exec

--- user/find.c
+++ user/find.c
@@ -3,12 +3,38 @@
 #include "user/user.h"
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
+#include "kernel/param.h"
+
+int m_argc = 0;
+char* cmd;
+char* m_argv[MAXARG];
 
 char* fmtname(char* path) {
 	char* p;
 	for (p = path + strlen(path); p >= path && *p != '/'; p--);
 	p++;
 	return p;
+}
+
+void exc(char* path) {
+	if (m_argc == 0) {
+		printf("%s\n", path);
+		return;
+	}
+	int p = fork();
+	if (p == 0) {
+		if (m_argc == MAXARG) {
+			fprintf(2, "command too long\n");
+			exit(1);
+		}
+		m_argv[m_argc] = path;
+		m_argv[m_argc + 1] = 0;
+		exec(cmd, m_argv);
+	} else if (p > 0) {
+		wait(0);
+	} else {
+		fprintf(2, "fork failed\n");
+	}
 }
 
 int find(char* path, char* dst) {
@@ -30,7 +56,7 @@ int find(char* path, char* dst) {
 	}
 
 	if (strcmp(fmtname(path), dst) == 0) {
-		printf("%s\n", path);
+		exc(path);
 		code = 0;
 	}
 
@@ -65,7 +91,7 @@ int find(char* path, char* dst) {
 		if (st.type == T_DIR) {
 			code = find(buf, dst);
 		} else if (!strcmp(de.name, dst)) {
-			printf("%s\n", buf);
+			exc(buf);
 			code = 0;
 		}
 	}
@@ -77,18 +103,34 @@ int find(char* path, char* dst) {
 int main(int argc, char* argv[]) {
 	int code = -1;
 
-	if (argc > 3 || argc < 2) {
+	if (argc < 2) {
 		fprintf(2, "Usage: find path filename\n");
 		exit(1);
 	}
 
+	if (argc > 3) {
+		if (argc < 5 || strcmp(argv[3], "-exec") != 0) {
+			fprintf(2, "Usage: find path filename -exec cmd ...\n");
+			exit(1);
+		}
+		m_argc = argc - 4;
+		if (m_argc + 1 >= MAXARG) {
+			fprintf(2, "find: too many exec arguments\n");
+			exit(1);
+		}
+
+		cmd = argv[4];
+		memcpy(m_argv, argv + 4, (argc - 4) * sizeof(char*));
+		m_argv[m_argc + 1] = 0;
+	}
+
 	if (argc == 2)
 		code = find(".", argv[1]);
-	if (argc == 3)
+	else
 		code = find(argv[1], argv[2]);
 
 	if (code == -1) {
-		printf("Cannot find the targent...\n");
+		printf("Cannot find the target...\n");
 	}
 
 	exit(0);
