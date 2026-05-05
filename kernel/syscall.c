85bedff099041c52d7b4efb45c7acd64bc76ba76
Louis Deng <louisaeilotd@gmail.com>
2026-05-05 11:10:28 +0800

    Update Interpose

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -90,6 +90,7 @@ extern uint64 sys_unlink(void);
 extern uint64 sys_link(void);
 extern uint64 sys_mkdir(void);
 extern uint64 sys_close(void);
+extern uint64 sys_interpose(void);
 
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
@@ -115,18 +116,24 @@ static uint64 (*syscalls[])(void) = {
     [SYS_link] sys_link,
     [SYS_mkdir] sys_mkdir,
     [SYS_close] sys_close,
+    [SYS_interpose] sys_interpose,
 };
 
 void syscall(void) {
 	int num;
 	struct proc* p = myproc();
+	int mask = p->mask;
 
 	num = p->trapframe->a7;
 	// num = *(int*)0;
 	if (num > 0 && num < NELEM(syscalls) && syscalls[num]) {
 		// Use num to lookup the system call function for num, call it,
 		// and store its return value in p->trapframe->a0
-		p->trapframe->a0 = syscalls[num]();
+		if ((mask & (1 << num)) == 0)
+			p->trapframe->a0 = syscalls[num]();
+		else {
+			p->trapframe->a0 = -1;
+		}
 	} else {
 		printf("%d %s: unknown sys call %d\n",
 		       p->pid, p->name, num);
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -8,100 +8,107 @@
 #include "vm.h"
 
 uint64
-sys_exit(void)
-{
-  int n;
-  argint(0, &n);
-  kexit(n);
-  return 0;  // not reached
+sys_exit(void) {
+	int n;
+	argint(0, &n);
+	kexit(n);
+	return 0;  // not reached
 }
 
 uint64
-sys_getpid(void)
-{
-  return myproc()->pid;
+sys_getpid(void) {
+	return myproc()->pid;
 }
 
 uint64
-sys_fork(void)
-{
-  return kfork();
+sys_fork(void) {
+	return kfork();
 }
 
 uint64
-sys_wait(void)
-{
-  uint64 p;
-  argaddr(0, &p);
-  return kwait(p);
+sys_wait(void) {
+	uint64 p;
+	argaddr(0, &p);
+	return kwait(p);
 }
 
 uint64
-sys_sbrk(void)
-{
-  uint64 addr;
-  int t;
-  int n;
+sys_sbrk(void) {
+	uint64 addr;
+	int t;
+	int n;
 
-  argint(0, &n);
-  argint(1, &t);
-  addr = myproc()->sz;
+	argint(0, &n);
+	argint(1, &t);
+	addr = myproc()->sz;
 
-  if(t == SBRK_EAGER || n < 0) {
-    if(growproc(n) < 0) {
-      return -1;
-    }
-  } else {
-    // Lazily allocate memory for this process: increase its memory
-    // size but don't allocate memory. If the processes uses the
-    // memory, vmfault() will allocate it.
-    if(addr + n < addr)
-      return -1;
-    myproc()->sz += n;
-  }
-  return addr;
+	if (t == SBRK_EAGER || n < 0) {
+		if (growproc(n) < 0) {
+			return -1;
+		}
+	} else {
+		// Lazily allocate memory for this process: increase its memory
+		// size but don't allocate memory. If the processes uses the
+		// memory, vmfault() will allocate it.
+		if (addr + n < addr)
+			return -1;
+		myproc()->sz += n;
+	}
+	return addr;
 }
 
 uint64
-sys_pause(void)
-{
-  int n;
-  uint ticks0;
+sys_pause(void) {
+	int n;
+	uint ticks0;
 
-  argint(0, &n);
-  if(n < 0)
-    n = 0;
-  acquire(&tickslock);
-  ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(killed(myproc())){
-      release(&tickslock);
-      return -1;
-    }
-    sleep(&ticks, &tickslock);
-  }
-  release(&tickslock);
-  return 0;
+	argint(0, &n);
+	if (n < 0)
+		n = 0;
+	acquire(&tickslock);
+	ticks0 = ticks;
+	while (ticks - ticks0 < n) {
+		if (killed(myproc())) {
+			release(&tickslock);
+			return -1;
+		}
+		sleep(&ticks, &tickslock);
+	}
+	release(&tickslock);
+	return 0;
 }
 
 uint64
-sys_kill(void)
-{
-  int pid;
+sys_kill(void) {
+	int pid;
 
-  argint(0, &pid);
-  return kkill(pid);
+	argint(0, &pid);
+	return kkill(pid);
 }
 
 // return how many clock tick interrupts have occurred
 // since start.
 uint64
-sys_uptime(void)
-{
-  uint xticks;
+sys_uptime(void) {
+	uint xticks;
 
-  acquire(&tickslock);
-  xticks = ticks;
-  release(&tickslock);
-  return xticks;
+	acquire(&tickslock);
+	xticks = ticks;
+	release(&tickslock);
+	return xticks;
+}
+
+uint64
+sys_interpose(void) {
+	int mask = 0;
+	argint(0, &mask);
+	myproc()->mask = mask;
+
+	uint64 uaddr;
+	char path[128];
+	argaddr(1, &uaddr);
+	if (copyinstr(myproc()->pagetable, path, uaddr, sizeof(path)) < 0)
+		return -1;
+
+	return myproc()->pid;
 }
